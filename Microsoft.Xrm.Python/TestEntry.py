8ed2c6a8b4a238863be40e897b32458f63bd22b6
Colton Lathrop <colathro@microsoft.com>
2018-10-19 09:01:10 -0500

    Added Authentication & acess token logic

--- Microsoft.Xrm.Python/TestEntry.py
+++ Microsoft.Xrm.Python/TestEntry.py
@@ -2,11 +2,13 @@ from Xrm import Xrm
 
 
 x = Xrm.ClientCredentials()
-x.CRMUrl = "test.com"
-x.Password = "password123"
+x.CRMUrl = "https://colton.api.crm.dynamics.com"
+x.Password = "Fargoctslabs!"
 x.Username = "admin@coltonlathrop.net"
 
 xx = Xrm.OrganizationServiceProxy()
 xx.ClientCredentials = x
 xx.Authenticate()
+
+print(xx.CheckToken())
 
--- Microsoft.Xrm.Python/Xrm/Xrm.py
+++ Microsoft.Xrm.Python/Xrm/Xrm.py
@@ -1,35 +1,96 @@
-import requests
 import json
+import time
+import requests
+
 
 class OrganizationServiceProxy():
     """
     Use: Used as the primary method for firing org side actions.
+
+    Attributes:
+        ClientCredentials: Set by user to define the context of the OrganizationServiceProxy.
+        AccessToken: Set when the request to the organization returns correctly.
+        AccessTokenExpiry: Unix time value of the time the token expires.
+        TokenEndpoint: Static URI string for microsoft online.
+        ClientId: Static GUID for the Dynamics CRM serice Global
     """
     ClientCredentials = None
-    Access_Token = None
-
+    AccessToken = None
+    AccessTokenExpiry = None
+    TokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token"
+    ClientId = "2ad88395-b77d-4561-9441-d0e40824f9bc" 
 
     def Authenticate(self):
+        """Authenticates the current passed ClientCredentials with microsoftonline/AAD
+
+        Args:
+            self: Authenticates it's own context.
+
+        Returns:
+            Nothing. It's expected that the user manually checks the token exists.
+
+        Raises:
+            AttributeError: Please define ClientCredentials or pass ClientCredentials as correct type.
+            BaseException: Access token is already set in this context.
+        """
         if self.ClientCredentials == None or type(self.ClientCredentials) is not ClientCredentials:
             raise AttributeError("Please define ClientCredentials or pass ClientCredentials as correct type.")
+        if self.AccessTokenExpiry != None:
+            raise BaseException("Access Token is already set in this context. Please check AccessTokenExpiry and call AuthenticateForce to force the reauth flow.")
+
+        WebformData = {
+            'client_id': self.ClientId,
+            'resource': self.ClientCredentials.CRMUrl,
+            'username': self.ClientCredentials.Username,
+            'password': self.ClientCredentials.Password,
+            'grant_type': 'password'
+        }
+        # Makes request and returns the json dictionary.
+        ResponseJson = self.ExtractSetJson(self.RequestToken(WebformData))
+
+        # Start trying to set the OrganizationServiceProxy Authentication context.
+        try:
+            self.AccessToken = ResponseJson["access_token"]
+            self.AccessTokenExpiry = ResponseJson["expires_on"]
+        except:
+            raise AttributeError("Unable to locate access_token or expires_on in response content.")
+
+    def ExtractSetJson(self, request):
+        return request.json()
+
+    
+    def RequestToken(self, data):
+        response = requests.post(self.TokenEndpoint, data=data)
+        if response.status_code == 200:
+            return response
+        else:
+            raise ConnectionError("Authentication has failed with response code: {0}".format([str(response.status_code)])+response.text)
+
+    def CheckToken(self):
+        """ Checks it's own token expiry to verify it's not expired, True = Good Token : False = Bad Token """
+        if self.AccessTokenExpiry == None:
+            return False
+        elif int(self.AccessTokenExpiry) < time.time():
+            return False
+        else: 
+            return True
+
+    def Create(self, entity):
         pass
 
-    def Create(entity):
+    def Delete(self, string, guid):
         pass
 
-    def Delete(string, guid):
+    def Execute(self, request):
         pass
 
-    def Execute(request):
+    def Retrieve(self, string, guid, columnSet):
         pass
 
-    def Retrieve(string, guid, columnSet):
+    def RetrieveMultiple(self, queryBase):
         pass
 
-    def RetrieveMultiple(queryBase):
-        pass
-
-    def Update(entity):
+    def Update(self, entity):
         pass
 
     pass
@@ -44,7 +105,7 @@ class ClientCredentials():
 
 class SecurityTokenResponse():
     """
-    Use: Used for the user to retreive the Oauth2 Security Token currently in user by OrganizationServiceProxy.
+    Use: Used for the user to retreive the Oauth2 Security Token currently in use by OrganizationServiceProxy.
     """
     pass
 
