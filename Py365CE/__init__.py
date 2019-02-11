ecc55ddde9590ca5b3230e3f037ff96248934ead
Colton Lathrop <colathro@microsoft.com>
2019-02-10 21:13:21 -0600

    place holder additional classes

--- Py365CE/__init__.py
+++ Py365CE/__init__.py
@@ -2,4 +2,7 @@
 from .auth import client_context
 from .create_record import create_record
 from .delete_record import delete_record
+from .update_record import update_record
+from .retrieve_record import retrieve_record
+from .retrieve_multiple import retrieve_multiple
 
---
+++ Py365CE/retrieve_multiple.py
@@ -0,0 +1,26 @@
+from .auth import * 
+import requests
+import json
+
+
+class retrieve_multiple():
+    
+    request_type = 0
+
+    def __init__(self, entity_type, auth_context, data=None):
+        '''Takes WebAPI entity identifier and an authcontext to build create.
+        
+        :param str entity_type: String of the records WebAPI entity identifier.
+        :param str auth_context: The user_context
+        :param str password: The password of the user named in the username
+            parameter.
+        '''
+        self.entity_type = entity_type
+        self.auth_context = auth_context
+        if data is not None:
+            self.data = data
+
+    @property
+    def build_create_url(self):
+        return self.auth_context.get_auth_value('resource') + self.auth_context.api_query_stem + self.entity_type
+
