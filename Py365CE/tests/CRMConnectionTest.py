bc215fa36e28315530406665e37d62ddde66fd2c
Colton Lathrop <colathro@microsoft.com>
2019-03-30 21:52:51 -0500

    gitignore

--- Py365CE/tests/CRMConnectionTest.py
+++
@@ -1,8 +0,0 @@
-from Py365CE import CRMConnection
-
-conn = CRMConnection('https://login.microsoftonline.com/coltonlathrop.net')
-conn.lazy_acquire_token_with_username_password('https://coltonlathrop.crm.dynamics.com', 'admin@coltonlathrop.net','Fargoctslabs!')
-
-print(list(conn.cache._cache.values())[0]['expiresOn'])
-print('x')
-
